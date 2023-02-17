0b8727f708c15d1f2d81ce149abb7a55193907af
Krunal234 <55957614+Krunal234@users.noreply.github.com>
2023-02-17 22:55:01 +0530

    implemented add and remove from cart and changed theme

--- 01-starting-project/src/store/CartProvider.js
+++ 01-starting-project/src/store/CartProvider.js
@@ -6,6 +6,7 @@ const defaultCartState = {
   totalAmount: 0,
 };
 const cartReducer = (state, action) => {
+  let updatedItems;
   if (action.type === "ADD_ITEM") {
     const updatedTotalAmount =
       state.totalAmount + action.item.price * action.item.amount;
@@ -14,7 +15,6 @@ const cartReducer = (state, action) => {
       (item) => item.id === action.item.id
     );
 
-    let updatedItems;
     if (existingItemIndex !== -1) {
       const existingItem = state.items[existingItemIndex];
       const updatedItem = {
@@ -25,6 +25,26 @@ const cartReducer = (state, action) => {
       updatedItems[existingItemIndex] = updatedItem;
     } else {
       updatedItems = state.items.concat(action.item);
+    }
+    return { items: updatedItems, totalAmount: updatedTotalAmount };
+  } else if (action.type === "REMOVE_ITEM") {
+    let updatedTotalAmount;
+    const existingItemIndex = state.items.findIndex(
+      (item) => item.id === action.id
+    );
+    if (existingItemIndex !== -1) {
+      const existingItem = state.items[existingItemIndex];
+      updatedTotalAmount = state.totalAmount - existingItem.price;
+      const updatedItem = {
+        ...existingItem,
+        amount: existingItem.amount - 1,
+      };
+      updatedItems = [...state.items];
+      updatedItems[existingItemIndex] = updatedItem;
+
+      if (updatedItem.amount <= 0) {
+        updatedItems.splice(existingItemIndex, 1);
+      }
     }
     return { items: updatedItems, totalAmount: updatedTotalAmount };
   }
@@ -40,7 +60,10 @@ const CartProvider = (props) => {
     dispatchCartAction({ type: "ADD_ITEM", item: item });
   };
 
-  const removeItemFromCartHandler = (id) => {};
+  const removeItemFromCartHandler = (id) => {
+    dispatchCartAction({ type: "REMOVE_ITEM", id: id });
+  };
+
   const cartContext = {
     items: cartState.items,
     totalAmount: cartState.totalAmount,
